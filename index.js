2e3f0cd2a3d7b1234681b1b2a0d1d89cbdbf1ecc
Hassan Talat <hassan.talat.92@gmail.com>
2016-02-24 07:20:09 +0000

    initial-crud-app

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose   = require('mongoose');
+var Item     = require('./app/models/item');
+
+
+
+mongoose.connect('mongodb://localhost:27017/db', function(err) {
+	if (err) {
+		console.error('Could not connect to MongoDB!');
+		console.log(err);
+	}else
+	    console.log("connected to the database");
+});
+
 
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -10,9 +23,89 @@ var port = process.env.PORT || 8080;
 
 var router = express.Router();
 
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+
 router.get('/',function(req,res){
     res.json({message: 'hoorayyy'});
 })
+
+router.route('/items')
+
+    // create a item (accessed at POST http://localhost:8080/api/items)
+    .post(function(req, res) {
+        
+        var item = new Item();      // create a new instance of the Item model
+        item.name = req.body.name;  // set the item name (comes from the request)
+        item.quantity = req.body.quantity;
+
+        // save the item and check for errors
+        item.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Item created!' });
+        });
+        
+    })
+    
+    .get(function(req,res){
+    	Item.find(function(err,items){
+    		if(err)
+    			res.send(err);
+    		
+    		res.json(items);
+    	});
+    });
+    
+    
+    router.route('items/:item_id')
+    	.get(function(req,res){
+    		Item.findById(req.params.item_id,function(err,item){
+    			if(err)
+    				res.send(err);
+    			res.json(item);
+    		});
+    	})
+    	
+    	.put(function(req, res) {
+
+        // use our item model to find the item we want
+        Item.findById(req.params.item_id, function(err, item) {
+
+            if (err)
+                res.send(err);
+
+            item.name = req.body.name; 
+            item.quantity = req.body.quantity; // update the items info
+
+            // save the item
+            item.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Item updated!' });
+            });
+
+        });
+    })
+    
+   .delete(function(req, res) {
+        Item.remove({
+            _id: req.params.item_id
+        }, function(err, item) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Successfully deleted' });
+        });
+    });
+
+    
 
 app.use('/api',router);
 
